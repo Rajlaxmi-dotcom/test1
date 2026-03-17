c94ef7cffe9f23b142e3ce0f9d553afc7f37d09f
Rajlaxmi-dotcom <rajlaxmi.d@quickheal.com>
2026-03-17 16:06:53 +0530

    Add files via upload

---
+++ seqrite-config.js
@@ -0,0 +1,696 @@
+
+let userLang = (navigator.language || (navigator).userLanguage || 'en');
+console.log("Browser Language Code: ", userLang);
+window.klaroConfig = {
+  "consentManagerId": "69b92db83752491a43c4cc43",
+  "uuid": "d943b174-382d-4f3c-8abd-c9c004bae217",
+  "domain": "https://dev-hs.qhtpl.com/cmapi",
+  "version": 1,
+  "elementID": "SEQRITE",
+  lang: userLang,
+  "storageMethod": "cookie",
+  "storageName": "https://www.starhealth.in/lp/health-insurance/dgnonbrand",
+  "cookieExpiresAfterDays": 365,
+  "default": false,
+  "mustConsent": true,
+  "acceptAll": true,
+  "hideDeclineAll": false,
+  "hideLearnMore": false,
+  "disablePoweredBy": false,
+  "translations": {
+    "english": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Others": {
+          "description": "<p>NA</p>"
+        },
+        "Functional": {
+          "description": "<p>NA</p>"
+        },
+        "Marketing": {
+          "description": "<p>NA</p>"
+        },
+        "Analytics": {
+          "description": "<p>NA</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "en": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Others": {
+          "description": "<p>NA</p>"
+        },
+        "Functional": {
+          "description": "<p>NA</p>"
+        },
+        "Marketing": {
+          "description": "<p>NA</p>"
+        },
+        "Analytics": {
+          "description": "<p>NA</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "zz": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Others": {
+          "description": "<p>NA</p>"
+        },
+        "Functional": {
+          "description": "<p>NA</p>"
+        },
+        "Marketing": {
+          "description": "<p>NA</p>"
+        },
+        "Analytics": {
+          "description": "<p>NA</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "optOut": {
+          "description": "Reject All",
+          "title": "Reject All"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "ta": {
+      "acceptAll": "அனைத்தையும் ஏற்றுக்கொள்ளுங்கள்",
+      "accept": "ஏற்றுக்கொள்ளுங்கள்",
+      "decline": "அனைத்தையும் நிராகரிக்கவும்",
+      "acceptSelected": "தேர்ந்தெடுக்கப்பட்டவர்களை ஏற்றுக்கொள்ளுங்கள்",
+      "ok": "அனைத்தையும் ஏற்றுக்கொள்ளுங்கள்",
+      "consentNotice": {
+        "description": "உங்கள் தனியுரிமையை நாங்கள் மதிக்கிறோம், அத்தியாவசிய தள செயல்பாட்டை செயல்படுத்த குக்கீகளை நாங்கள் சேமிக்கிறோம், அதே போல் சந்தைப்படுத்தல், தனிப்பயனாக்கம் மற்றும் பகுப்பாய்வு. \"அனைத்தையும் ஏற்றுக்கொள்ளுங்கள்\" என்பதைக் கிளிக் செய்வதன் மூலம், எங்கள் குக்கீகளைப் பயன்படுத்த நீங்கள் ஒப்புக்கொள்கிறீர்கள். நீங்கள் எந்த நேரத்திலும் உங்கள் அமைப்புகளை மாற்றலாம். குக்கீ கொள்கை.",
+        "learnMore": "தனிப்பயனாக்குங்கள்",
+        "changeDescription": " "
+      },
+      "save": "ஏற்றுக்கொள்ளுங்கள்",
+      "consentModal": {
+        "title": "சம்மத முன்னுரிமைகளைத் தனிப்பயனாக்குங்கள்",
+        "description": "பக்கங்களுக்கு இடையில் திறம்பட செல்லவும், உங்கள் விருப்பங்களை சேமிக்கவும், பொதுவாக ஒரு வலைத்தளத்தின் உங்கள் அனுபவத்தை மேம்படுத்தவும் குக்கீகளைப் பயன்படுத்துகிறோம். இருப்பினும், உங்கள் தனியுரிமைக்கான உரிமையை நாங்கள் ஒப்புக்கொள்கிறோம், எனவே சில வகையான குக்கீகளை அனுமதிக்க வேண்டாம் என்று நீங்கள் தேர்வு செய்யலாம். மேலும் தெரிந்துகொள்ளவும், நமது இயல்புநிலை அமைப்புகளை மாற்றவும் வெவ்வேறு வகைத் தலைப்புகளைக் கிளிக் செய்க. சில வகையான குக்கீகளைத் தடுப்பது தளத்தின் உங்கள் அனுபவத்தை பாதிக்கக்கூடும் என்பதை நினைவில் கொள்க.",
+        "buttons": {
+          "save": "ஏற்றுக்கொள்ளுங்கள்",
+          "acceptAll": "அனைத்தையும் ஏற்றுக்கொள்ளுங்கள்",
+          "decline": "அனைத்தையும் நிராகரிக்கவும்"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Others": {
+          "description": "என்ஏ"
+        },
+        "Functional": {
+          "description": "என்ஏ"
+        },
+        "Marketing": {
+          "description": "என்ஏ"
+        },
+        "Analytics": {
+          "description": "என்ஏ"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "அனைத்தையும் நிராகரிக்கவும்",
+          "title": "அனைத்தையும் நிராகரிக்கவும்"
+        },
+        "optOut": {
+          "description": "அனைத்தையும் நிராகரிக்கவும்",
+          "title": "அனைத்தையும் நிராகரிக்கவும்"
+        },
+        "purpose": "நோக்கம்"
+      },
+      "purposeItem": {
+        "service": "சேவை",
+        "services": "சேவைகள்"
+      }
+    }
+  },
+  "styling": {
+    "theme": [
+      "light",
+      "custom"
+    ]
+  },
+  "htmlTexts": true,
+  "embedded": false,
+  "declarative": false,
+  "groupByPurpose": true,
+  "hideToggleAll": false,
+  "appName": "testbanner",
+  "description": "We respects your privacy and is committed to protecting your personal data.",
+  "companyName": "testbanner",
+  "companyAddress": " ",
+  "services": [
+    {
+      "name": "bm_sz",
+      "title": "bm_sz",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "bm_sz"
+      ]
+    },
+    {
+      "name": "_dyjsession",
+      "title": "_dyjsession",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_dyjsession"
+      ]
+    },
+    {
+      "name": "__Host-next-auth.csrf-token",
+      "title": "__Host-next-auth.csrf-token",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "__Host-next-auth.csrf-token"
+      ]
+    },
+    {
+      "name": "__Secure-next-auth.callback-url",
+      "title": "__Secure-next-auth.callback-url",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "__Secure-next-auth.callback-url"
+      ]
+    },
+    {
+      "name": "bm_sv",
+      "title": "bm_sv",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "bm_sv"
+      ]
+    },
+    {
+      "name": "ak_bmsc",
+      "title": "ak_bmsc",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "ak_bmsc"
+      ]
+    },
+    {
+      "name": "_gcl_au",
+      "title": "_gcl_au",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_gcl_au"
+      ]
+    },
+    {
+      "name": "_dyid",
+      "title": "_dyid",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_dyid"
+      ]
+    },
+    {
+      "name": "_dy_soct",
+      "title": "_dy_soct",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_dy_soct"
+      ]
+    },
+    {
+      "name": "CLID",
+      "title": "CLID",
+      "purposes": [
+        "Analytics"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "CLID"
+      ]
+    },
+    {
+      "name": "_ga",
+      "title": "_ga",
+      "purposes": [
+        "Analytics"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_ga"
+      ]
+    },
+    {
+      "name": "IDE",
+      "title": "IDE",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "IDE"
+      ]
+    },
+    {
+      "name": "_clck",
+      "title": "_clck",
+      "purposes": [
+        "Analytics"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_clck"
+      ]
+    },
+    {
+      "name": "_uetsid",
+      "title": "_uetsid",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_uetsid"
+      ]
+    },
+    {
+      "name": "_uetvid",
+      "title": "_uetvid",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_uetvid"
+      ]
+    },
+    {
+      "name": "MUID",
+      "title": "MUID",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "MUID"
+      ]
+    },
+    {
+      "name": "MR",
+      "title": "MR",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "MR"
+      ]
+    },
+    {
+      "name": "neo_sc",
+      "title": "neo_sc",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "neo_sc"
+      ]
+    },
+    {
+      "name": "nt_user_id",
+      "title": "nt_user_id",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "nt_user_id"
+      ]
+    },
+    {
+      "name": "nt_trait",
+      "title": "nt_trait",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "nt_trait"
+      ]
+    },
+    {
+      "name": "nt_group_id",
+      "title": "nt_group_id",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "nt_group_id"
+      ]
+    },
+    {
+      "name": "nt_group_trait",
+      "title": "nt_group_trait",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "nt_group_trait"
+      ]
+    },
+    {
+      "name": "nt_anonymous_id",
+      "title": "nt_anonymous_id",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "nt_anonymous_id"
+      ]
+    },
+    {
+      "name": "nt_page_init_referrer",
+      "title": "nt_page_init_referrer",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "nt_page_init_referrer"
+      ]
+    },
+    {
+      "name": "nt_page_init_referring_domain",
+      "title": "nt_page_init_referring_domain",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "nt_page_init_referring_domain"
+      ]
+    },
+    {
+      "name": "neo_session",
+      "title": "neo_session",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "neo_session"
+      ]
+    },
+    {
+      "name": "_clsk",
+      "title": "_clsk",
+      "purposes": [
+        "Analytics"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_clsk"
+      ]
+    },
+    {
+      "name": "SRM_B",
+      "title": "SRM_B",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "SRM_B"
+      ]
+    },
+    {
+      "name": "SM",
+      "title": "SM",
+      "purposes": [
+        "Analytics"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "SM"
+      ]
+    },
+    {
+      "name": "ANONCHK",
+      "title": "ANONCHK",
+      "purposes": [
+        "Marketing"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "ANONCHK"
+      ]
+    },
+    {
+      "name": "_abck",
+      "title": "_abck",
+      "purposes": [
+        "Functional"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_abck"
+      ]
+    },
+    {
+      "name": "_ga_J59EYF8C12",
+      "title": "_ga_J59EYF8C12",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": false,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_ga_J59EYF8C12"
+      ]
+    }
+  ]
+};
+showCookie();
